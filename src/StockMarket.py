e0ccc31f6fc52d3b42ef357979fc3b47a4aeeef3
Jeppe Udesen <jeppeudesen@Jeppes-MacBook-Air.local>
2016-09-19 11:14:46 +0200

    Added daterolling + non-functional trade method

--- src/StockMarket.py
+++ src/StockMarket.py
@@ -1,6 +1,7 @@
 import quandl
 from Stock import Stock
 import pandas as pd
+from datetime import timedelta
 
 # quandl api authentication
 quandl.ApiConfig.api_key = '-b2fk-v3un2si3_FB18U'
@@ -16,8 +17,21 @@ class StockMarket:
     def stocks(self):
         return self.m_stocks
 
-    def history(self,start,end):
+    def history(self, start, end):
         req_stocks = [x.code() + ".4" for x in self.stocks()]
         data = quandl.get(req_stocks, start_date=start, end_date=end)
         data = data.dropna(axis = 1)
         return data
+        
+    def daterolling(start, end):
+        for n in range(int((end - start).days)):
+            single_date = start + timedelta(n)
+            
+            #print(single_date.strftime("%Y-%m-%d"))
+            yield single_date
+    
+    def trade(stocks, start, end):
+        trading_stocks = [x.code() + ".4" for x in self.stocks()]
+        for single_date in (StockMarket.daterolling(start, end)):      
+            prices = quandl.get(trading_stocks, start_date=single_date, end_date=single_date)
+            print(prices)
--- src/gran.py
+++ src/gran.py
@@ -1,16 +1,20 @@
 from StockMarket import StockMarket
 import PairTrading
+from datetime import date
 
 stocks_path = "data/WIKI_subset.csv"
 
 sm = StockMarket(stocks_path)
-sh = sm.history("2015-06-01","2016-01-01")
+#sh = sm.history("2015-06-01","2016-01-01")
 # print(sh.corr())
-
 #print(sh)
 #print(PairTrading.example())
 
-print(PairTrading.compute_pairs(sh))
+#print(PairTrading.compute_pairs(sh))
+start_date = date(2015, 5, 1)
+end_date = date(2015, 6, 2)
+#print(StockMarket.daterange(start_date, end_date))
+print(StockMarket.trade(sm, start_date, end_date))
 
 # clean_sh = sm.clean_history(sh)
 # print(clean_sh.corr())
